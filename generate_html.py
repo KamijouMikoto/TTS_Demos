b25df57bc16f47f933df2c6ef8b2a6648c1881d3
Zijiang Yang <zijiang.yang@informatik.uni-augsburg.de>
2021-06-21 02:07:11 +0200

    Added demos for 22/06/21

--- generate_html.py
+++ generate_html.py
@@ -21,8 +21,9 @@ def get_file_list():
                 'neu': sorted([file for file in all_files if 'neu' in file and original_file in file]),
                 'CycleGAN': sorted([file for file in all_files if 'cyc' in file and original_file in file]),
                 'VAW-GAN': sorted([file for file in all_files if 'vaw' in file and original_file in file]),
-                'Phoneme_embedded VAW-GAN': sorted([file for file in all_files if 'phoneme' in file and original_file in file]),
-                'CMU-MOSEI': sorted([file for file in all_files if 'cmu-mosei' in file and original_file in file])
+                'Phoneme embedded VAW-GAN': sorted([file for file in all_files if 'phoneme' in file and original_file in file]),
+                'CMU-MOSEI': sorted([file for file in all_files if 'cmu-mosei' in file and original_file in file]),
+                'VAW-GAN with forced alignment on word-level': sorted([file for file in all_files if 'vaw-fa-word' in file and original_file in file])
             }
     return files_by_folder
 
@@ -61,9 +62,10 @@ def generate_html():
         with a.body():
             with a.div(klass="container pb-5 pt-5"):
                 a.h1(_t="Emotional TTS Demo Files", klass="text-center display-1 pt-5 pb-5")
-                a.h5(_t="19/04/21: Reproduced VAW-GAN based emotion converter", klass="pt-5 pb-3")
+                a.h5(_t="19/04/21  : Reproduced VAW-GAN based emotion converter", klass="pt-5 pb-3")
                 a.h5(_t="01/06/21-A: Added phoneme embeddings to the encoder of VAW-GAN", klass="pt-5 pb-3")
                 a.h5(_t="01/06/21-B: Tested CMU-MOSEI with the vanilla VAW-GAN", klass="pt-5 pb-3")
+                a.h5(_t="22/06/21  : Tested VAW-GAN with forced alignment on word-level", klass="pt-5 pb-3")
                 for folder, files_by_file_name in get_file_list().items():
                     a.h3(_t=f"Samples from {convert_folder_name_to_date(folder)}", klass="text-center display-6 pt-5")
                     for original_file_name, files_by_model in files_by_file_name.items():
